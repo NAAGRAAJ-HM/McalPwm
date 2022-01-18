6ca0f31e8532e7013bcfa300e514b5fed293803c
root <nagaraja.hm.0001@gmail.com>
2022-01-18 09:06:21 +0100

    Update: submodule Pwm

--- LATEST/Pwm.cpp
+++ LATEST/Pwm.cpp
@@ -5,3 +5,42 @@ class_Pwm Pwm;
 FUNC(void, PWM_CODE) class_Pwm::InitFunction(void){
 }
 
+FUNC(void, PWM_CODE) class_Pwm::DeInitFunction(void){
+}
+
+FUNC(void, PWM_CODE) class_Pwm::SetDutyCycle(void){
+}
+
+FUNC(void, PWM_CODE) class_Pwm::SetPeriodAndDuty(void){
+}
+
+FUNC(void, PWM_CODE) class_Pwm::SetOutputToIdle(void){
+}
+
+FUNC(void, PWM_CODE) class_Pwm::SetOutputState(void){
+}
+
+FUNC(void, PWM_CODE) class_Pwm::DisableNotification(void){
+}
+
+FUNC(void, PWM_CODE) class_Pwm::EnableNotification(void){
+}
+
+FUNC(void, PWM_CODE) class_Pwm::SetPowerState(void){
+}
+
+FUNC(void, PWM_CODE) class_Pwm::GetCurrentPowerState(void){
+}
+
+FUNC(void, PWM_CODE) class_Pwm::GetTargetPowerState(void){
+}
+
+FUNC(void, PWM_CODE) class_Pwm::PreparePowerState(void){
+}
+
+FUNC(void, PWM_CODE) class_Pwm::GetVersionInfo(void){
+}
+
+FUNC(void, PWM_CODE) class_Pwm::MainFunction(void){
+}
+
--- LATEST/Pwm.h
+++ LATEST/Pwm.h
@@ -5,7 +5,20 @@
 
 class class_Pwm{
    public:
-      FUNC(void, PWM_CODE) InitFunction(void);
+      FUNC(void, PWM_CODE) InitFunction         (void);
+      FUNC(void, PWM_CODE) DeInitFunction       (void);
+      FUNC(void, PWM_CODE) SetDutyCycle         (void);
+      FUNC(void, PWM_CODE) SetPeriodAndDuty     (void);
+      FUNC(void, PWM_CODE) SetOutputToIdle      (void);
+      FUNC(void, PWM_CODE) SetOutputState       (void);
+      FUNC(void, PWM_CODE) DisableNotification  (void);
+      FUNC(void, PWM_CODE) EnableNotification   (void);
+      FUNC(void, PWM_CODE) SetPowerState        (void);
+      FUNC(void, PWM_CODE) GetCurrentPowerState (void);
+      FUNC(void, PWM_CODE) GetTargetPowerState  (void);
+      FUNC(void, PWM_CODE) PreparePowerState    (void);
+      FUNC(void, PWM_CODE) GetVersionInfo       (void);
+      FUNC(void, PWM_CODE) MainFunction         (void);
 };
 
 extern class_Pwm Pwm;
