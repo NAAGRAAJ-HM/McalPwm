ee0abaed9d1dafd59fbdfd600ad367ee50750c54
root <nagaraja.hm.0001@gmail.com>
2022-03-28 12:47:36 +0200

    Update: AUTOSAR basic software micro-controller abstraction module Pwm

--- LATEST/Pwm.cpp
+++ LATEST/Pwm.cpp
@@ -90,7 +90,19 @@ FUNC(void, PWM_CODE) module_Pwm::GetVersionInfo(void){
 FUNC(void, PWM_CODE) module_Pwm::MainFunction(void){
 }
 
-#include "Pwm_Unused.hpp"
+class class_Pwm_Unused{
+   public:
+      FUNC(void, PWM_CODE) SetDutyCycle         (void);
+      FUNC(void, PWM_CODE) SetPeriodAndDuty     (void);
+      FUNC(void, PWM_CODE) SetOutputToIdle      (void);
+      FUNC(void, PWM_CODE) SetOutputState       (void);
+      FUNC(void, PWM_CODE) DisableNotification  (void);
+      FUNC(void, PWM_CODE) EnableNotification   (void);
+      FUNC(void, PWM_CODE) SetPowerState        (void);
+      FUNC(void, PWM_CODE) GetCurrentPowerState (void);
+      FUNC(void, PWM_CODE) GetTargetPowerState  (void);
+      FUNC(void, PWM_CODE) PreparePowerState    (void);
+};
 
 FUNC(void, PWM_CODE) class_Pwm_Unused::SetDutyCycle(void){
 }
