5ff3e31d4750ef060b7a1fc944cd6add8e663a52
root <nagaraja.hm.0001@gmail.com>
2022-04-06 13:00:03 +0200

    Update: AUTOSAR basic software micro-controller abstraction module Pwm

--- LATEST/Pwm.cpp
+++ LATEST/Pwm.cpp
@@ -14,18 +14,18 @@
 /******************************************************************************/
 /* #DEFINES                                                                   */
 /******************************************************************************/
-#define PWM_AR_RELEASE_MAJOR_VERSION                                           4
-#define PWM_AR_RELEASE_MINOR_VERSION                                           3
+#define PWM_AR_RELEASE_VERSION_MAJOR                                           4
+#define PWM_AR_RELEASE_VERSION_MINOR                                           3
 
 /******************************************************************************/
 /* MACROS                                                                     */
 /******************************************************************************/
-#if(PWM_AR_RELEASE_MAJOR_VERSION != STD_AR_RELEASE_MAJOR_VERSION)
-   #error "Incompatible PWM_AR_RELEASE_MAJOR_VERSION!"
+#if(PWM_AR_RELEASE_VERSION_MAJOR != STD_AR_RELEASE_VERSION_MAJOR)
+   #error "Incompatible PWM_AR_RELEASE_VERSION_MAJOR!"
 #endif
 
-#if(PWM_AR_RELEASE_MINOR_VERSION != STD_AR_RELEASE_MINOR_VERSION)
-   #error "Incompatible PWM_AR_RELEASE_MINOR_VERSION!"
+#if(PWM_AR_RELEASE_VERSION_MINOR != STD_AR_RELEASE_VERSION_MINOR)
+   #error "Incompatible PWM_AR_RELEASE_VERSION_MINOR!"
 #endif
 
 /******************************************************************************/
@@ -63,8 +63,10 @@ CONSTP2VAR(infSchMClient, PWM_VAR, PWM_CONST) gptrinfSchMClient_Pwm = &Pwm;
 /******************************************************************************/
 VAR(module_Pwm, PWM_VAR) Pwm(
    {
-         0x0000
-      ,  0xFFFF
+         PWM_AR_RELEASE_VERSION_MAJOR
+      ,  PWM_AR_RELEASE_VERSION_MINOR
+      ,  0x00
+      ,  0xFF
       ,  0x01
       ,  '0'
       ,  '1'
