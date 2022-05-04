91b7e98d3d2b06ad3a60264754142838d871f338
NAGARAJA HM <nagaraja.hm.0001@gmail.com>
2022-05-04 09:01:37 +0000

    Update: AUTOSAR basic software micro-controller abstraction driver module Pwm

--- LATEST/Pwm.cpp
+++ LATEST/Pwm.cpp
@@ -113,7 +113,7 @@ FUNC(void, PWM_CODE) module_Pwm::InitFunction(
             0 //TBD: IdModule
          ,  0 //TBD: IdInstance
          ,  0 //TBD: IdApi
-         ,  0 //TBD: IdError
+         ,  PWM_E_UNINIT
       );
 #endif
    }
@@ -133,7 +133,7 @@ FUNC(void, PWM_CODE) module_Pwm::DeInitFunction(void){
             0 //TBD: IdModule
          ,  0 //TBD: IdInstance
          ,  0 //TBD: IdApi
-         ,  0 //TBD: IdError
+         ,  PWM_E_UNINIT
       );
 #endif
    }
@@ -152,7 +152,7 @@ FUNC(void, PWM_CODE) module_Pwm::MainFunction(void){
             0 //TBD: IdModule
          ,  0 //TBD: IdInstance
          ,  0 //TBD: IdApi
-         ,  0 //TBD: IdError
+         ,  PWM_E_UNINIT
       );
 #endif
    }
