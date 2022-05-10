09937612113861857b983189d71864d46b86cd50
NAGARAJA HM <nagaraja.hm.0001@gmail.com>
2022-05-10 10:09:43 +0000

    Update: AUTOSAR basic software micro-controller abstraction driver module Pwm

--- LATEST/Pwm.cpp
+++ LATEST/Pwm.cpp
@@ -89,7 +89,10 @@ FUNC(void, PWM_CODE) module_Pwm::InitFunction(
    CONSTP2CONST(CfgModule_TypeAbstract, PWM_CONFIG_DATA, PWM_APPL_CONST) lptrCfgModule
 ){
 #if(STD_ON == Pwm_InitCheck)
-   if(E_OK != IsInitDone){
+   if(
+         E_OK
+      != IsInitDone
+   ){
 #endif
       if(NULL_PTR != lptrCfgModule){
          if(STD_HIGH){
@@ -127,7 +130,10 @@ FUNC(void, PWM_CODE) module_Pwm::InitFunction(
 
 FUNC(void, PWM_CODE) module_Pwm::DeInitFunction(void){
 #if(STD_ON == Pwm_InitCheck)
-   if(E_OK == IsInitDone){
+   if(
+         E_OK
+      == IsInitDone
+   ){
 #endif
 #if(STD_ON == Pwm_InitCheck)
       IsInitDone = E_NOT_OK;
@@ -147,7 +153,10 @@ FUNC(void, PWM_CODE) module_Pwm::DeInitFunction(void){
 
 FUNC(void, PWM_CODE) module_Pwm::MainFunction(void){
 #if(STD_ON == Pwm_InitCheck)
-   if(E_OK == IsInitDone){
+   if(
+         E_OK
+      == IsInitDone
+   ){
 #endif
 #if(STD_ON == Pwm_InitCheck)
    }
