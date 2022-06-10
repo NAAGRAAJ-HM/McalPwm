df57e547209f1bd40989e19866091e8f01955acf
NAGARAJA HM <nagaraja.hm.0001@gmail.com>
2022-06-10 13:33:45 +0000

    Update: AUTOSAR basic software micro-controller abstraction driver module Pwm

--- LATEST/Pwm.cpp
+++ LATEST/Pwm.cpp
@@ -73,10 +73,6 @@ CONSTP2VAR(infSchMClient, PWM_VAR, PWM_CONST) gptrinfSchMClient_Pwm = &Pwm;
 /******************************************************************************/
 VAR(module_Pwm, PWM_VAR) Pwm(
    {
-#if(STD_ON == _ReSIM)
-         "Pwm",
-#else
-#endif
          PWM_AR_RELEASE_VERSION_MAJOR
       ,  PWM_AR_RELEASE_VERSION_MINOR
       ,  0x00
