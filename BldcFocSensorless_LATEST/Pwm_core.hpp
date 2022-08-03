0d09adc7d8139108b13402ffe9a8cede8f80a264
Nagaraja HM <nagaraja.hm.0001@gmail.com>
2022-08-03 17:37:10 +0200

    Update: AUTOSAR basic software micro-controller abstraction driver module for pulse width modulation

---
+++ BldcFocSensorless_LATEST/Pwm_core.hpp
@@ -0,0 +1,70 @@
+#pragma once
+/******************************************************************************/
+/* File   : Pwm_core.hpp                                                      */
+/* Author : NAGARAJA HM (c) since 1982. All rights reserved.                  */
+/******************************************************************************/
+
+/******************************************************************************/
+/* #INCLUDES                                                                  */
+/******************************************************************************/
+#include "CompilerCfg_Pwm.hpp"
+
+/******************************************************************************/
+/* #DEFINES                                                                   */
+/******************************************************************************/
+#define PWM_CORE_FUNCTIONALITIES                                               \
+              FUNC(void, PWM_CODE) SetDutyCycle         (void);                \
+              FUNC(void, PWM_CODE) SetPeriodAndDuty     (void);                \
+              FUNC(void, PWM_CODE) SetOutputToIdle      (void);                \
+              FUNC(void, PWM_CODE) SetOutputState       (void);                \
+              FUNC(void, PWM_CODE) DisableNotification  (void);                \
+              FUNC(void, PWM_CODE) EnableNotification   (void);                \
+              FUNC(void, PWM_CODE) SetPowerState        (void);                \
+              FUNC(void, PWM_CODE) GetCurrentPowerState (void);                \
+              FUNC(void, PWM_CODE) GetTargetPowerState  (void);                \
+              FUNC(void, PWM_CODE) PreparePowerState    (void);                \
+
+#define PWM_CORE_FUNCTIONALITIES_VIRTUAL                                       \
+      virtual FUNC(void, PWM_CODE) SetDutyCycle         (void) = 0;            \
+      virtual FUNC(void, PWM_CODE) SetPeriodAndDuty     (void) = 0;            \
+      virtual FUNC(void, PWM_CODE) SetOutputToIdle      (void) = 0;            \
+      virtual FUNC(void, PWM_CODE) SetOutputState       (void) = 0;            \
+      virtual FUNC(void, PWM_CODE) DisableNotification  (void) = 0;            \
+      virtual FUNC(void, PWM_CODE) EnableNotification   (void) = 0;            \
+      virtual FUNC(void, PWM_CODE) SetPowerState        (void) = 0;            \
+      virtual FUNC(void, PWM_CODE) GetCurrentPowerState (void) = 0;            \
+      virtual FUNC(void, PWM_CODE) GetTargetPowerState  (void) = 0;            \
+      virtual FUNC(void, PWM_CODE) PreparePowerState    (void) = 0;            \
+
+/******************************************************************************/
+/* MACROS                                                                     */
+/******************************************************************************/
+
+/******************************************************************************/
+/* TYPEDEFS                                                                   */
+/******************************************************************************/
+class class_Pwm_Functionality{
+   public:
+      PWM_CORE_FUNCTIONALITIES_VIRTUAL
+};
+
+/******************************************************************************/
+/* CONSTS                                                                     */
+/******************************************************************************/
+
+/******************************************************************************/
+/* PARAMS                                                                     */
+/******************************************************************************/
+
+/******************************************************************************/
+/* OBJECTS                                                                    */
+/******************************************************************************/
+
+/******************************************************************************/
+/* FUNCTIONS                                                                  */
+/******************************************************************************/
+
+/******************************************************************************/
+/* EOF                                                                        */
+/******************************************************************************/
+
