57f7e9990da5004ffc8c2cede1c2c8b9fb488a8b
Nagaraja HM <nagaraja.hm.0001@gmail.com>
2023-01-24 21:52:17 +0100

    Update: AUTOSAR basic software micro-controller abstraction driver module for pulse width modulation

---
+++ InfineonTLE9879QXA40_R2023/infMcalPwm_ServiceNvM.hpp
@@ -0,0 +1,19 @@
+#pragma once
+/******************************************************************************/
+/* File   : infMcalPwm_ServiceNvM.hpp                                         */
+/* Author : NAGARAJA HM (c) since 1982. All rights reserved.                  */
+/******************************************************************************/
+
+/******************************************************************************/
+/* #DEFINES                                                                   */
+/******************************************************************************/
+#define CONSTMCALPWM_AR_RELEASE_VERSION_MAJOR                                  4
+#define CONSTMCALPWM_AR_RELEASE_VERSION_MINOR                                  3
+
+#define Const_DefaultInit_ConstMcalPwm                                                                           \
+   ,  {CONSTMCALPWM_AR_RELEASE_VERSION_MAJOR, CONSTMCALPWM_AR_RELEASE_VERSION_MINOR, 0x00,0xFF,0x01,'0','1','0'} \
+
+/******************************************************************************/
+/* EOF                                                                        */
+/******************************************************************************/
+
